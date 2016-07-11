c7dda0db75898065a712c0717d40516dbf651740
William Shields <williamshields@dhcp232.pp.rhul.ac.uk>
2016-07-11 15:43:49 +0100

    Update to the reader. Include ability to read standard output as well as beam output.

--- reader.py
+++ reader.py
@@ -1,8 +1,50 @@
 import numpy as _np
+import pybdsim
 
 class reader():
-    def get_data(self,file):
-        ''' Returns a dictionary of parameters from the input file.
+    def get_data(self,file,type=None,num_lines=15):
+        if isinstance(type,_np.str):
+            if type == 'beam':
+                transdata = self.get_beam_output(file)
+                return transdata
+            elif type == 'standard':
+                transdata = self.get_standard_output(file)
+                return transdata
+    
+        f = open(file)
+        flist=[]
+        transdata = None
+        for i in range(num_lines):
+            line = f.next()
+            if line[-1] == '\n':
+                line = line[:-1]
+            if line[-1] == '\r':
+                line = line[:-1]
+            flist.append(line)
+            splitline = self._remove_blanks(line.split(' '))
+            if splitline[0] == '*BEAM*':     ## Is beam output
+                #print "'*BEAM*' found in line " + _np.str(i+1)
+                f.close()
+                transdata = self.get_beam_output(file)
+                break
+            elif line == '0    0':
+                #print "'0    0' found in line " + _np.str(i+1)
+                f.close()
+                transdata = self.get_standard_output(file)
+                break
+            else:
+                pass
+        if transdata == None:
+            errorstring =  "Could not find an indicator in the first "+_np.str(num_lines)+" lines of the input file for either\n"
+            errorstring += "a beam output file (indicator = '*BEAM*), or a standard output file (indicator = '0    0').\n"
+            errorstring += "Please check the input file or specify the input type with the type argument in the get_output function.\n"
+            errorstring += "Note that the only accepted values for type are 'standard' or 'beam'."
+            raise IOError(errorstring)
+        return transdata
+
+
+    def get_beam_output(self,file):
+        ''' Returns a BDSAsciiData instance of parameters from the input file.
             The input file is assumed to contain the beam data as output 
             manually by TRANSPORT. As such, the structure of said output 
             is assumed to remain unchanged from lattice to lattice. This code 
@@ -34,8 +76,16 @@ class reader():
                 that a direct dispersion comparison to another lattice may appear incorrect.
             '''
         flist = self._file_to_list(file)
-        params = self._process_data(flist)
-        return params
+        transdata = self.process_beam_output(flist)
+        return transdata
+  
+  
+    def get_standard_output(self,file):
+        flist = self._file_to_list(file)
+        lattice,output = self._get_latticeandoutput(flist)
+        transdata = self._process_elements(output)
+        return transdata
+    
 
     def _file_to_list(self,file):
         ''' Converts the input file into a list. The data has to be in a format other than
@@ -45,9 +95,11 @@ class reader():
             by line. This may be an inefficent method but the input file should not be very 
             large so it should not require a large amount of memory.
             '''
+        self.file = file
         num_lines = _np.sum(1 for line in open(file))
         flist=[]
         infile = open(file)
+        ##Loop over lines and remove any carriage returns
         for i in range(num_lines):
             line = infile.next()
             if line[-1] == '\n':
@@ -59,27 +111,212 @@ class reader():
         return flist
 
 
-    def _process_data(self,flist):
-        params = {
-            'sigx'      :[],
-            'sigxp'     :[],
-            'sigy'      :[],
-            'sigyp'     :[],
-            's'         :[],
-            'alfx'      :[],
-            'alfy'      :[],
-            'betx'      :[],
-            'bety'      :[],
-            'ex'        :[],
-            'ey'        :[],
-            'dx'        :[],
-            'dy'        :[],
-            'sigp'      :[],
-            'type'      :[],
-            'name'      :[],
-            'code'      :'transport'
-            }
+    def _get_lattice(self,flist):
+        ''' Function to extract the lattice from a standard output file. No processing at the moment, but
+            this function should identify the chunk that is the lattice.
+            '''
+        foundlatticestart = False
+        foundlatticeend = False
 
+        for linenum,line in enumerate(flist):
+            if line == '0    0':
+                if not foundlatticestart:
+                    latticestart = linenum+1
+                    foundlatticestart = True
+            if line == '0SENTINEL':
+                if not foundlatticeend:
+                    latticeend = linenum
+                    foundlatticeend = True
+        if not foundlatticestart:
+            if not foundlatticeend:
+                raise IOError('No lattice found in '+self.file+'.')
+            else:
+                errorstring  = 'The end of a lattice (line = "0SENTINEL") was found at line '+ _np.str(latticeend+1)+',\n'
+                errorstring += 'but the start of a lattice (line = "0    0") was not found. Please check the input file.'
+                raise IOError(errorstring)
+        elif not foundlatticeend:
+                errorstring  = 'The start of a lattice (line = "0    0") was found at line '+ _np.str(latticestart-1)+',\n'
+                errorstring += 'but the end of a lattice (line = "0SENTINEL") was not found. Please check the input file.'
+                raise IOError(errorstring)
+        else:
+            lattice = flist[latticestart:latticeend]
+        return lattice
+
+
+    def _get_output(self,flist):
+        ''' Function to extract the output from a standard output file. The output will be an list of the lines
+            for each element which conatins the beam data. Each element should contain the R and TRANSPORT matrices which
+            are necessary so the beam info can be calculated.
+            '''
+        foundoutputstart = False
+        foundoutputend = False
+
+        for linenum,line in enumerate(flist):
+            if (line == '') and (linenum < (len(flist)-1)):
+                nextline = flist[linenum+1]
+                if (self._remove_blanks(nextline.split(' '))[0] == '*BEAM*') and not foundoutputstart:
+                    outputstart=linenum+1
+                    foundoutputstart = True
+            if self._remove_blanks(line.split(' '))[0] == '0*LENGTH*':
+                if not foundoutputend:
+                    outputend = linenum
+                    foundoutputend = True
+        if not foundoutputstart:
+            if not foundoutputend:
+                raise IOError('No output found in '+self.file+'.')
+            else:
+                errorstring  = 'The end of a lattice (line containing "0*LENGTH*") was found at line '+ _np.str(outputend+1)+',\n'
+                errorstring += 'but the start of a lattice (first line containing "*BEAM*") was not found. Please check the input file.'
+                raise IOError(errorstring)
+        elif not foundoutputend:
+                errorstring  = 'The start of a lattice (first line containing "*BEAM*") was found at line '+ _np.str(outputstart-1)+',\n'
+                errorstring += 'but the end of a lattice (line containing "0*LENGTH*") was not found. Please check the input file.'
+                raise IOError(errorstring)
+        else:
+            output = flist[outputstart:outputend]
+
+        #Split the list of all element data into their individual elements.
+        elementlist=[]
+        elementstart = False
+        elementend = False
+        finalelement = False
+        for linenum,line in enumerate(output):
+            if linenum == (len(output)-1):
+                finalelement = True
+            try:
+                if line[1] == '*':
+                    if line[2:11] == 'TRANSFORM':   #Is midway through element output
+                        pass
+                    elif elementstart == False: #Current line must be start of the element
+                        elementstart = True
+                        elementstartline = linenum
+                    elif elementstart == True:
+                        elementend = True       #Otherwise the line must be the start of the next element
+                        elementendline = linenum
+                if elementstart and finalelement:
+                    element = output[elementstartline:]
+                    elementlist.append(element)
+                if elementstart and elementend: #If the start and end of the element are found, append and reset
+                    element = output[elementstartline:elementendline]
+                    elementlist.append(element)
+                    elementend = False
+                    elementstart = False
+                if elementstart == False and elementend == False:   #Though if it's been reset, it must be because the
+                    elementstart = True                             #current line is the start of next element, so set the
+                    elementstartline = linenum                      #start line for the next element
+            except IndexError:
+                pass
+        return elementlist
+
+
+    def _get_latticeandoutput(self,flist):
+        lattice = self._get_lattice(flist)
+        output = self._get_output(flist)
+        return lattice,output
+    
+
+    def _process_elements(self,elementlist):
+        transdata = {
+            'Sigma_x'   :[],
+            'Sigma_xp'  :[],
+            'Sigma_y'   :[],
+            'Sigma_yp'  :[],
+            'S'         :[],
+            'Alph_x'    :[],
+            'Alph_y'    :[],
+            'Beta_x'    :[],
+            'Beta_y'    :[],
+            'Emitt_x'   :[],
+            'Emitt_y'   :[],
+            'Disp_x'    :[],
+            'Disp_y'    :[],
+            'Sigma_p'   :[],
+            'Name'      :[],
+            }
+        for element in elementlist:
+            if len(element) > 1:  # I.e not a fit or matrix-modifying element
+                type    = self._remove_blanks(element[0].split(' '))[0].strip('*')
+                s       = _np.float(self._remove_blanks(element[1].split(' '))[0])
+                sigx    = _np.float(self._remove_blanks(element[1].split(' '))[3])
+                sigxp   = _np.float(self._remove_blanks(element[2].split(' '))[1])
+                sigy    = _np.float(self._remove_blanks(element[3].split(' '))[1])
+                sigyp   = _np.float(self._remove_blanks(element[4].split(' '))[1])
+                sigt    = _np.float(self._remove_blanks(element[5].split(' '))[1])
+                sigp    = _np.float(self._remove_blanks(element[6].split(' '))[1])
+
+                r21     = _np.float(self._remove_blanks(element[2].split(' '))[3])
+                r43     = _np.float(self._remove_blanks(element[4].split(' '))[5])
+                dx      = _np.float(self._split_negatives(self._remove_blanks(element[8].split(' ')))[5])
+                dy      = _np.float(self._split_negatives(self._remove_blanks(element[10].split(' ')))[5])
+
+                xpint   = _np.sqrt(sigxp**2 * (1 - r21**2))
+                ypint   = _np.sqrt(sigyp**2 * (1 - r43**2))
+                
+                ex      = sigx*xpint
+                ey      = sigy*ypint
+                betx    = sigx**2 / ex
+                bety    = sigy**2 / ey
+                gammax  = sigxp**2 / ex
+                gammay  = sigyp**2 / ey
+                alfx    = _np.sqrt((gammax * betx)-1)
+                alfy    = _np.sqrt((gammax * betx)-1)
+
+                transdata['Sigma_x'].append(sigx/1000)
+                transdata['Sigma_xp'].append(sigxp)
+                transdata['Sigma_y'].append(sigy/1000)
+                transdata['Sigma_yp'].append(sigyp)
+                transdata['S'].append(s)
+                transdata['Alph_x'].append(alfx)
+                transdata['Alph_y'].append(alfy)
+                transdata['Beta_x'].append(betx)
+                transdata['Beta_y'].append(bety)
+                transdata['Emitt_x'].append(ex)
+                transdata['Emitt_y'].append(ey)
+                transdata['Disp_x'].append(dx)
+                transdata['Disp_y'].append(dy)
+                transdata['Sigma_p'].append(sigp)
+
+        num_elements = len(elementlist)
+
+        def get_elementdata(index):             # Function to get the data for each element, rather than each key.
+            elementlist2=[]                      # There's probably a better container type for this, but I'm familiar with
+            for name in transdata.keys():   # dictionaries, and it works (for now).
+                if name == 'Name':
+                    elementlist2.append(' ')
+                else:
+                    elementlist2.append(transdata[name][index])
+        
+            return elementlist2
+
+        data = pybdsim.Data.BDSAsciiData()      # Now convert the dict into BDSAsciiData instance for final output.
+        for name in transdata.keys():
+            data._AddProperty(name)
+        for i in range(num_elements):
+            data.append(get_elementdata(i))
+
+        return data
+
+
+
+    def process_beam_output(self,flist):
+        transdata = {
+            'Sigma_x'   :[],
+            'Sigma_xp'  :[],
+            'Sigma_y'   :[],
+            'Sigma_yp'  :[],
+            'S'         :[],
+            'Alph_x'    :[],
+            'Alph_y'    :[],
+            'Beta_x'    :[],
+            'Beta_y'    :[],
+            'Emitt_x'   :[],
+            'Emitt_y'   :[],
+            'Disp_x'    :[],
+            'Disp_y'    :[],
+            'Sigma_p'   :[],
+            'Name'      :[],
+            }
+        num_elements = 0
         for elenum,element in enumerate(flist):
             if element == '':   ### The first line of the section should be a blank line.
                 try:
@@ -92,40 +329,40 @@ class reader():
                     reststr = line0split[2]
                     restsplit = reststr.split(' ')
                     restsplit = self._remove_blanks(restsplit)
-                    params['s'].append(_np.float(restsplit[2]))
+                    transdata['S'].append(_np.float(restsplit[2]))
                     namestr = restsplit[4]
-                    params['name'].append(namestr)
-                    params['type'].append(typestr)
+                    transdata['Name'].append(namestr)
+                    #transdata['type'].append(typestr)
 
                     ### Get sigma_x and sigma_xp
                     line3 = section[3].split(' ')
                     line3 = self._remove_blanks(line3)
-                    params['sigx'].append(_np.float(line3[3])/1000)
-                    params['sigxp'].append(_np.float(line3[5])/1000)
+                    transdata['Sigma_x'].append(_np.float(line3[3])/1000)
+                    transdata['Sigma_xp'].append(_np.float(line3[5])/1000)
 
                     ### Get sigma_y and sigma_yp
                     line4 = section[4].split(' ')
                     line4 = self._remove_blanks(line4)
-                    params['sigy'].append(_np.float(line4[3])/1000)
-                    params['sigyp'].append(_np.float(line4[5])/1000)
+                    transdata['Sigma_y'].append(_np.float(line4[3])/1000)
+                    transdata['Sigma_yp'].append(_np.float(line4[5])/1000)
                     
                     ### Get momentum spread
                     line5 = section[5].split(' ')
-                    params['sigp'].append(_np.float(line5[5])/100)
+                    transdata['Sigma_p'].append(_np.float(line5[5])/100)
                     
-                    ### Get alfa and beta twiss params for x and y.
+                    ### Get alfa and beta twiss transdata for x and y.
                     line7 = section[7].split(' ')
                     line7 = self._remove_blanks(line7)
-                    params['alfx'].append(_np.float(line7[0]))
-                    params['betx'].append(_np.float(line7[1]))
-                    params['alfy'].append(_np.float(line7[3]))
-                    params['bety'].append(_np.float(line7[4]))
+                    transdata['Alph_x'].append(_np.float(line7[0]))
+                    transdata['Beta_x'].append(_np.float(line7[1]))
+                    transdata['Alph_y'].append(_np.float(line7[3]))
+                    transdata['Beta_y'].append(_np.float(line7[4]))
 
                     ### Get horizontal and vertical dispersion
                     line10 = section[10].split(' ')
                     line10 = self._remove_blanks(line10)
-                    params['dx'].append(_np.float(line10[2])/10)
-                    params['dy'].append(_np.float(line10[5])/10)
+                    transdata['Disp_x'].append(_np.float(line10[2])/10)
+                    transdata['Disp_y'].append(_np.float(line10[5])/10)
                     
                     ### Terms for calculating the emittance.
                     term1x = _np.float(line3[3])**2
@@ -136,19 +373,29 @@ class reader():
                     ### Get horizontal and vertical emittance
                     emittx = (term1x - term2x)/ _np.float(line7[1])
                     emitty = (term1y - term2y)/ _np.float(line7[4])
-                    params['ex'].append(emittx)
-                    params['ey'].append(emitty)
+                    transdata['Emitt_x'].append(emittx)
+                    transdata['Emitt_y'].append(emitty)
                 
+                    num_elements += 1
                 except ValueError:
                     errstr = "Could not process section beginning at line " + _np.str(elenum) + " : "
                     print(errstr)
                     print " "
                     print(element)
-            
-        for key in params.keys():
-            params[key] = _np.array(params[key])
 
-        return params
+        def get_elementdata(index):             # Function to get the data for each element, rather than each key.
+            elementlist=[]                      # There's probably a better container type for this, but I'm familiar with
+            for name in transdata.keys():   # dictionaries, and it works (for now).
+                elementlist.append(transdata[name][index])
+            return elementlist
+
+        data = pybdsim.Data.BDSAsciiData()      # Now convert the dict into BDSAsciiData instance for final output.
+        for name in transdata.keys():
+            data._AddProperty(name)
+        for i in range(num_elements):
+            data.append(get_elementdata(i))
+
+        return data
 
 
     def _remove_blanks(self,line):
@@ -162,8 +409,17 @@ class reader():
         stripline = newline.split(' ')
         return stripline
 
-
-
+    def _split_negatives(self,line):
+        newline=[]
+        for element in line:
+            negpos = element.find('-')
+            if negpos > 1:
+                parts = element.split('-')
+                newline.append(parts[0])
+                newline.append('-'+parts[1])
+            else:
+                newline.append(element)
+        return newline
 
 
 
